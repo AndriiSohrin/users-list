3a7f82416dbf37a0e5fcd980dc33fd87ee7b5f38
AndriiSokhryn_Eleph <andriisokhryn@elephantslab.com>
2024-05-21 15:21:15 +0300

    Added Saved Buttons in Details

--- pages/details/[id].vue
+++ pages/details/[id].vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import type {UserInterface} from "~/types/User.interface";
+import Buttons from "~/components/User/Buttons.vue";
+import {useStore} from "~/store/main.store";
 const route = useRoute();
 const router = useRouter();
 const { data: user, pending } = await useFetch<UserInterface>('https://dummyjson.com/users/' + route.params.id);
+
+const store = useStore()
 
 if (!user.value) {
   throw createError({
     statusCode: 404,
     message: `User with ID of ${route.params.id} not found`
   });
+}
+
+const isAlreadySaved = computed(() => !!store.users[Number(route.params.id)])
+
+
+function save() {
+  if(user.value) {
+    store.saveUser(user.value)
+  }
 }
 
 const goBack = () => {
@@ -19,16 +32,22 @@ const goBack = () => {
 <template>
   <div>
     <button @click="goBack"  class="absolute -left-24 top-5 bg-slate-500 text-white px-3 py-1 rounded">Back</button>
+
     <div v-if="pending">Loading...</div>
     <div v-if="user" class="bg-white shadow-md rounded-lg overflow-hidden">
       <div class="p-6">
-        <div class="flex items-center space-x-4">
-          <img :src="user.image" alt="User Avatar" class="w-24 h-24 rounded-full object-cover">
-          <div>
-            <h2 class="text-2xl font-bold">{{ user.firstName }} {{ user.lastName }}</h2>
-            <p class="text-gray-600">@{{ user.username }}</p>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center space-x-4">
+            <img :src="user.image" alt="User Avatar" class="w-24 h-24 rounded-full object-cover">
+            <div>
+              <h2 class="text-2xl font-bold">{{ user.firstName }} {{ user.lastName }}</h2>
+              <p class="text-gray-600">@{{ user.username }}</p>
+            </div>
           </div>
+
+         <UserButtons :isAlreadySaved="isAlreadySaved" @save="save" />
         </div>
+
         <div class="mt-8">
           <h3 class="text-xl font-bold mb-4">Contact Information</h3>
           <div class="border border-gray-300 rounded-lg p-4 mb-4">
